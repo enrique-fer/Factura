fcdc093da3096f144b10ecd5b48d9c67cee99976
Christian <christiamigo@gmail.com>
2020-05-20 17:41:46 +0200

    Descomentar atributo estado del proyecto Factura.java

--- Factura/src/Factura.java
+++ Factura/src/Factura.java
@@ -2,7 +2,7 @@ public class Factura {
 	public int num;
 	public float base;
         public float tipoIva;
-        //public Estados estado;
+        public Estados estado;
 
 	public static void main(String[] args) {
 
